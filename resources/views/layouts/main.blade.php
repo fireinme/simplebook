008df586b10d8787b78c27a9d12bace04474914d
dennis <1016144734grc@gmail.com>
2017-10-21 20:55:58 +0800

    first simple book

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,11 @@
+@include('layouts._head')
+<div class="container">
+    <div class="blog-header">
+    </div>
+    <div class="row">
+        @yield('content')
+        @include('layouts._side')
+    </div><!-- /.row -->
+</div><!-- /.container -->
+
+@include('layouts._foot')
